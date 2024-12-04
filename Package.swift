73e2564d7e0bfe9f3c7705d74c6cce07725115a0
yume190 <yume190@gmail.com>
2024-12-04 19:21:08 +0800

    Updated for release SwiftUIX_1.0.2

--- Package.swift
+++ Package.swift
@@ -1,1 +1,34 @@
-1.0.2
+// swift-tools-version:5.10
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "SwiftUIX",
+    products: [
+        .library(
+            name: "SwiftUIX",
+            targets: ["_SwiftUIX_Aggregation", "SwiftUIX_Aggregation"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "_SwiftUIX_Aggregation",
+            dependencies: [.target(name: "_SwiftUIX")]
+        ),
+        .binaryTarget(
+            name: "_SwiftUIX",
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/210988081.zip",
+            checksum: "93f3ac5bfb564f6f3c9b61130ebf639ce58037dc7f3815ecc399a7f4d5896c73"
+        ),
+        .target(
+            name: "SwiftUIX_Aggregation",
+            dependencies: [.target(name: "SwiftUIX"), "_SwiftUIX_Aggregation"]
+        ),
+        .binaryTarget(
+            name: "SwiftUIX",
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/210988088.zip",
+            checksum: "d800a20e7c1b331a04f68c54433c9fef0fb67cea1ce5e0982129fadfa95e72cf"
+        )
+    ]
+)
