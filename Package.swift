295ff347298088d9ab9402c2ae0f9c0563510f86
yume190 <yume190@gmail.com>
2024-12-04 17:12:27 +0800

    Updated for release SwiftUIX_1.0.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "_SwiftUIX",
-            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/210958207.zip",
-            checksum: "aae015726f1e61e77005f8150ec39b0f0d9ef1b1adbbbfd653108f4bc03582bf"
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/210959806.zip",
+            checksum: "9484432201604d88fe6a910cd4e09f737a5ed6e154ab4963037371ddae1db484"
         ),
         .target(
             name: "SwiftUIX_Aggregation",
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SwiftUIX",
-            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/210958214.zip",
-            checksum: "a12b66774648ca90e0e8bf7b9c4241d5ff046a6ffe0e546ee4e45e3d49b15581"
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/210959819.zip",
+            checksum: "4f58f49912dafd44ee9e1c67e64377cf091b52de4ec6db8e2a67e63d978edc5c"
         )
     ]
 )
