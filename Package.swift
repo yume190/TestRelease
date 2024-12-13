51654fc5d7784a77bc2c57d54770555b72ea3b8a
yume190 <yume190@gmail.com>
2024-12-13 20:58:54 +0800

    Updated for release Engine_1.9.5

--- Package.swift
+++ Package.swift
@@ -1,1 +1,97 @@
-1.0.2
+// swift-tools-version:5.9
+
+import PackageDescription
+import Foundation
+import CompilerPluginSupport
+
+let package = Package(
+    name: "Engine",
+    platforms: [
+        .iOS("13.0"),
+        .macCatalyst("13.0"),
+        .macOS("10.15"),
+        .tvOS("13.0"),
+        .visionOS("1.0"),
+        .watchOS("6.0")
+    ],
+    products: [
+        .library(
+            name: "Engine",
+            targets: ["Engine_Aggregation"]
+        ),
+        .library(
+            name: "EngineCore",
+            targets: ["EngineCore_Aggregation"]
+        ),
+        .library(
+            name: "EngineCoreC",
+            targets: ["EngineCoreC_Aggregation"]
+        ),
+        .library(
+            name: "EngineMacros",
+            targets: ["EngineMacros_Aggregation"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/swiftlang/swift-syntax",
+            "509.0.0"..<"600.1.1"
+        )
+    ],
+    targets: [
+        .target(
+            name: "Engine_Aggregation",
+            dependencies: [.target(name: "Engine"), "EngineCore_Aggregation"]
+        ),
+        .binaryTarget(
+            name: "Engine",
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/213122864.zip",
+            checksum: "5a582eba74a94e2b6b00014c2100bfdb3017a0d75bc0c01af056e0ba120117c1"
+        ),
+        .target(
+            name: "EngineCore_Aggregation",
+            dependencies: [.target(name: "EngineCore"), "EngineCoreC_Aggregation"]
+        ),
+        .binaryTarget(
+            name: "EngineCore",
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/213122868.zip",
+            checksum: "170ca8606de39a701dd644a5767a27c5d34a4aa8cea4684333eea117278f6a0c"
+        ),
+        .target(
+            name: "EngineCoreC_Aggregation",
+            dependencies: [.target(name: "EngineCoreC")]
+        ),
+        .binaryTarget(
+            name: "EngineCoreC",
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/213122871.zip",
+            checksum: "747f34c0b27fd14339724bcfb4b0f50543d5bc31ee7cb4a85cf24ba7cf0b8739"
+        ),
+        .target(
+            name: "EngineMacros_Aggregation",
+            dependencies: [
+                .target(name: "EngineMacros"),
+                "Engine_Aggregation",
+                "EngineMacrosCore"
+            ]
+        ),
+        .binaryTarget(
+            name: "EngineMacros",
+            url: "https://api.github.com/repos/yume190/TestRelease/releases/assets/213122872.zip",
+            checksum: "4fdff07b28f3637f18b1cb74a4f665f38a4fab5dc3e9876158d56da41d96e031"
+        ),
+        .macro(
+            name: "EngineMacrosCore",
+            dependencies: [
+                .product(
+                    name: "SwiftSyntaxMacros",
+                    package: "swift-syntax"
+                ),
+                .product(
+                    name: "SwiftCompilerPlugin",
+                    package: "swift-syntax"
+                )
+            ],
+            path: "Macros/EngineMacrosCore"
+        )
+    ]
+)
